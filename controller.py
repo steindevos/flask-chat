9f33f6aaf84ce43ea54a7f5a8c23cd447c1e56bb
SDV <steindevos@gmail.com>
2018-07-24 09:43:15 +0000

    list can be updated through form

--- controller.py
+++ controller.py
@@ -12,5 +12,9 @@ messages = [
 def show_index():
     return render_template("index.html", messages = messages)
     
+@app.route("/add", methods = ["POST"])
+def show_add():
+    messages.append(request.form["message"])
+    return redirect("/")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
