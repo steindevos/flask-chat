dbc89f6b34fae63f08d46079776a9a57be0a8028
SDV <steindevos@gmail.com>
2018-07-24 09:03:51 +0000

    chat messages come from list

--- controller.py
+++ controller.py
@@ -2,9 +2,15 @@ from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
+messages = [
+    "Stein: whasdfs", 
+    "Piet: sdflkjsdf",
+    "Henk: nooo!"
+    ]
+    
 @app.route("/")
 def show_index():
-    return render_template("index.html")
+    return render_template("index.html", messages = messages)
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
